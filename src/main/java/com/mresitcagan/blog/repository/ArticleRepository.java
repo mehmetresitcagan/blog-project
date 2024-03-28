c2d940044c318a93eefecd0243c338e0676649e4
mehmetresitcagan <mresitcagan@gmail.com>
2024-03-28 12:16:01 +0300

    added repository layer

---
+++ src/main/java/com/mresitcagan/blog/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.mresitcagan.blog.repository;
+
+import com.mresitcagan.blog.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
---
+++ src/main/java/com/mresitcagan/blog/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mresitcagan.blog.repository;
+
+import com.mresitcagan.blog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
