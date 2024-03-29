db70be323cb7b00a324fd357d08f5ce629c7c6fc
mehmetresitcagan <mresitcagan@gmail.com>
2024-03-29 20:48:47 +0300

    update security config

--- src/main/java/com/mresitcagan/blog/config/WebSecurityConfig.java
+++ src/main/java/com/mresitcagan/blog/config/WebSecurityConfig.java
@@ -3,36 +3,51 @@ package com.mresitcagan.blog.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.net.http.HttpRequest;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
-    private static final String[] WHITELIST = {
-            "/",
-            "/register",
-            "/login",
-            "/h2-console"
-    };
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Note: spring security requestMatchers updated again
+        // https://stackoverflow.com/questions/76809698/spring-security-method-cannot-decide-pattern-is-mvc-or-not-spring-boot-applicati
         http
-                .authorizeRequests()
-                .requestMatchers(WHITELIST).permitAll()
-                .requestMatchers(HttpMethod.GET, "/posts/*").permitAll()
-                .anyRequest().authenticated();
-        
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(antMatcher("/css/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/js/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/images/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/fonts/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/webjars/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/")).permitAll();
+                    auth.requestMatchers(antMatcher("/rss/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/register/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/posts/**")).permitAll();
+                    auth.requestMatchers(PathRequest.toH2Console()).permitAll();
+                    auth.anyRequest().authenticated();
+                })
+
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error")
+                        .permitAll()
+                );
+
+
         return http.build();
     }
-
 }
